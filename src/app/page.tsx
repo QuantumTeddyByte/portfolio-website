8003a3a01aa35258cbf566665b5a327909945991
tadmax-unix <69086725+tadmax-unix@users.noreply.github.com>
2024-06-01 03:16:02 -0700

    Fixed wording on the main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <p className="mt-2 text-lg sm:text-xl">Software Engineer</p>
       <div className="mt-8 text-base sm:text-lg">
         <p>
-          Hi, my name is Teddy Malhan, software engineer based in Burnaby, BC.
+          Hi, my name is Teddy Malhan, and I'm a CS freshman at Simon Fraser University, BC, Canada.
         </p>
 
         <br />
