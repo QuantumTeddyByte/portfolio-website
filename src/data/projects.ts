200c00036776d5f9a39aba0ae9a77c5cc0dbab7a
tadmax-unix <69086725+tadmax-unix@users.noreply.github.com>
2024-06-06 14:49:35 -0700

    Updated URL

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,17 +1,26 @@
 const PROJECTS: {
-  name: string
-  description: string
-  previewImage: string
-  repoUrl: string
-  liveLink: string
+	name: string;
+	description: string;
+	previewImage: string;
+	repoUrl: string;
+	liveLink: string;
 }[] = [
-  {
-    name: 'PotHolePatrol',
-    description: 'Real-Time Pothole Tracking for BC with Secure Driver’s License Sign-In.',
-    liveLink: 'https://potholepatrol.co',
-    previewImage: '/project1alternate.png',
-    repoUrl: 'https://github.com/QuantumTeddyByte/pot-holes',
-  },
-]
+	{
+		name: "GradGains (Hack-The-Sem Winner)",
+		description: "Financial Social Media for University Students.",
+		liveLink:
+			"https://www.linkedin.com/posts/teddy-m-0a42a4257_hackathonvictory-professionalgrowth-innovativedevelopment-activity-7191941011097411584-wRqJ?utm_source=share&utm_medium=member_desktop",
+		previewImage: "/project2.png",
+		repoUrl: "https://github.com/QuantumTeddyByte/grad-gains",
+	},
+	{
+		name: "PotHolePatrol",
+		description:
+			"Real-Time Pothole Tracking for BC with Secure Driver’s License Sign-In.",
+		liveLink: "https://potholepatrol.co",
+		previewImage: "/project1alternate.png",
+		repoUrl: "https://github.com/QuantumTeddyByte/pot-holes",
+	},
+];
 
-export default PROJECTS
+export default PROJECTS;
